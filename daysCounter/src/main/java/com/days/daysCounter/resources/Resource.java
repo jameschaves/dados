b6aaf7c4ab8bbb3abd49e95d8d1ef10207223989
lucasgandac <lucasgandac@hotmail.com>
2020-08-18 19:36:33 -0300

    Subindo arquivo que estava incompleto

--- daysCounter/src/main/java/com/days/daysCounter/resources/Resource.java
+++ daysCounter/src/main/java/com/days/daysCounter/resources/Resource.java
@@ -1,21 +1,8 @@
 package com.days.daysCounter.resources;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
-
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.w3c.dom.Document;
-
-import com.days.daysCounter.DaysCounterApplication;
-
-import daysCounter.models.Days;
 import daysCounter.repository.daysRepository;
 
 @RestController
@@ -23,8 +10,8 @@ import daysCounter.repository.daysRepository;
 public class Resource {
 	
 	@GetMapping(path = "/datas")
-	public String getDias(@RequestParam String data1 , @RequestParam String data2 )
+	public int getDias(@RequestParam String data1 , @RequestParam String data2 )
 	{
-		return daysRepository.XMLopener();
+		return daysRepository.XMLopener(data1, data2);
 	}
 }
