b707b2b231980d381f6955e76bf4f09587a19943
lucasgandac <lucasgandac@hotmail.com>
2020-08-27 17:45:09 -0300

    Adicionando funcionalidade de ver proximo dia util

--- daysCounter/src/main/java/com/days/daysCounter/resources/Resource.java
+++ daysCounter/src/main/java/com/days/daysCounter/resources/Resource.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import daysCounter.repository.daysRepository;
+import daysCounter.repository.uteisRepository;
 
 @RestController
 @RequestMapping(value="api")
@@ -13,5 +14,11 @@ public class Resource {
 	public int getDias(@RequestParam String data1 , @RequestParam String data2 )
 	{
 		return daysRepository.XMLopener(data1, data2);
+	}
+
+	@GetMapping(path = "/uteis")
+	public String getUteis(@RequestParam String data1)
+	{
+		return uteisRepository.BusinessCalculator(data1);
 	}
 }
